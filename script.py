d7ba88e0c49ea236464f43821ead984a235a3dc3
Howard DaCosta <howard.dacosta400@gmail.com>
2022-03-31 19:09:21 -0400

    video controls, opening applications with speech
    Still a little slow, sometimes have to repeat phrase multiple times even in quiet environments. Will investigate different Speech APIs as there seems to be no other way around this.

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from pynput.keyboard import Key, Controller
 import speech_recognition as sr
+import pyautogui
 keyboard = Controller()
 
 # new window / tab
@@ -64,34 +65,80 @@ def recognize_audio(r, mic):
     return transcript
 
 
-def contains_url(transcript):
-    words = transcript.split(' ')
+def contains_url(words):
     for w in words:
-        if ".com" in w:
+        if any(domain in w for domain in [".com", ".edu", ".org"]):
             return w
     return ""
 
+def video_control(words, is_skip=False):
+    # requires headphones!
+    print("coming into video controls!")
+    # extract number from transcript
+    ten_sec_increment = 0
+    five_sec_increment = 0
+    for w in words:
+        if w.isdigit():
+            amount = int(w)
+            ten_sec_increment = amount // 10
+            amount -= 10 * ten_sec_increment
+            if amount > 5:
+                five_sec_increment = amount // 5
+            break
+    if is_skip:
+        print("skipping! 10s / 5s:", ten_sec_increment, five_sec_increment)
+        for _ in range(ten_sec_increment):
+            print("pressing l")
+            keyboard.tap('l')
+        for _ in range(five_sec_increment):
+            keyboard.tap(Key.right)
+    else:
+        for _ in range(ten_sec_increment):
+            keyboard.tap('j')
+        for _ in range(five_sec_increment):
+            keyboard.tap(Key.left) 
+
+
 def scrape_transcript_for_commands(transcript):
-    print("tab" in transcript)
+    transcript = transcript.lower()
+    words = transcript.split(" ")
+    # application controls
+    if "type" in transcript:
+        keyboard.type(' '.join(words[1:])) # assuming phrase is "type <phrase>" 
+    if "open" in transcript:
+        with keyboard.pressed(Key.cmd): # open spotlight search, only for MACs
+            keyboard.tap(Key.space)
+        # for some reason pynput does not work in spotlight search?
+        pyautogui.typewrite(' '.join(words[1:])) # assuming phrase is "open <app>"
+        keyboard.tap(Key.enter)
+
+    # browser controls
     if "tab" in transcript:
         print("tab in transcript")
         with keyboard.pressed(Key.cmd):
-            keyboard.press('t')
-            keyboard.release('t') 
+            keyboard.tap('t')
         print('pressed keyboard')
     if "window" in transcript:
         with keyboard.pressed(Key.cmd):
-            keyboard.press('n')
-            keyboard.release('n')
+            keyboard.tap('n')
+
+    # video controls
+    if any(word in transcript for word in ["skip", "forward", "fast forward"]):
+        video_control(words, is_skip=True)
+    
+    if any(word in transcript for word in ["rewind", "back", "go back"]):
+        video_control(transcript, is_skip=False)
+    
+    if any(word in transcript for word in ["play", "pause", "stop"]):
+        keyboard.tap('k')
+
     else:
-        url = contains_url(transcript)
+        url = contains_url(words)
         if url != "":
             with keyboard.pressed(Key.cmd):
-                keyboard.press('l')
-                keyboard.release('l')
-
+                keyboard.tap('l')
             keyboard.type(url)
-            keyboard.press(Key.enter)
+            keyboard.tap(Key.enter)
 
 
 if __name__ == "__main__":
